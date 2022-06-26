9f8378a70b5092e111fc325277e3b1609ff900b2
tommygun1974 <106986833+tommygun1974@users.noreply.github.com>
2022-06-26 14:28:55 +1000

    Second commit, fixed bug Fixed bug with search when no data recieved from search bar.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 
 import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
@@ -7,50 +7,56 @@ import "./App.css";
 const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    searchMovies("batman");
-  }, []);
+    useEffect(() => {
+        searchMovies("batman");
+    }, []);
 
-  const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
-    const data = await response.json();
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
 
-    setMovies(data.Search);
-  };
+        if (data.Response == "False") {
+            data.Search = "";
+        }
+        setMovies(data.Search);
+    };
 
-  return (
-    <div className="app">
-      <h1>MovieLand</h1>
+    return (
+        <div className="app">
+            <h1>MovieLand</h1>
 
-      <div className="search">
-        <input 
-          value= {searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Search for movies"
-        />
-        <img
-          src={SearchIcon}
-          alt="search"
-          onClick={ () => searchMovies(searchTerm)}
-        />
-      </div>
-      {movies.length > 0 ? (
-        <div className="container">
-          {movies.map((movie) => (
-            <MovieCard movie={movie} />
-          ))}
+            <div className="search">
+                <input value={searchTerm}
+                    onChange={
+                        (e) => setSearchTerm(e.target.value)
+                    }
+                    placeholder="Search for movies"/>
+                <img src={SearchIcon}
+                    alt="search"
+                    onClick={
+                        () => searchMovies(searchTerm)
+                    }/>
+            </div>
+
+            {
+                movies.length > 0 ? (
+                <div className="container">
+                    {
+                    movies.map((movie) => (
+                    <MovieCard movie={movie}/>))
+                    } 
+                </div>
+            ) : (
+                <div className="empty">
+                    <h2>No movies found</h2>
+                </div>
+                 )
+            } 
         </div>
-      ) : (
-        <div className="empty">
-          <h2>No movies found</h2>
-        </div>
-      )}
-      
-    </div>
-  );
+    );
 };
 
 export default App;
